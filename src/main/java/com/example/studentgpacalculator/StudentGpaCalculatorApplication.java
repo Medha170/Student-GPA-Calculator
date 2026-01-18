c03c4853e3ce7b7d8886af721ca6ba6d3021a464
Medha Shree <medha.23bcs10049@sst.scaler.com>
2026-01-18 14:57:38 +0530

    init

---
+++ src/main/java/com/example/studentgpacalculator/StudentGpaCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentgpacalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGpaCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentGpaCalculatorApplication.class, args);
+    }
+
+}
